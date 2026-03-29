dca0926fdd710ca6b7d367627c50e09f1fc288af
dean <jerekitibwa@gmail.com>
2026-03-29 17:17:42 +0300

    This code shows the application UI design
    This Main.java is being used to show the the UI design for both GridLayoutsExample.java and LayoutsDemo.java

---
+++ Main.java
@@ -0,0 +1,10 @@
+package Layouts;
+
+import java.awt.BorderLayout;
+
+public class Main {
+    public static void main(String[] args) {
+        GridLayoutExample example = new GridLayoutExample();
+        example.createFrame();
+    }
+}
